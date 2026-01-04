4d6e6aa7ac93f9963884d12b9c0cf739fc7890d5
Emilio <emilio93erv@gmail.com>
2026-01-04 10:23:21 +0100

    Frontend mínimo conectado por Socket.IO

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" }
 });
+
+// ⬇️ SERVIR FRONTEND
+app.use(express.static(path.join(__dirname, "../client")));
 
 io.on("connection", (socket) => {
   console.log("Jugador conectado:", socket.id);
